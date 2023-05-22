cf938f35433a9100075ac7075c908fc8615b1ec2
Togosling <toga.abdyldaev@gmail.com>
2023-05-22 15:35:48 +0800

    Initial Commit

---
+++ Neobis_iOS_Auth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Auth
+//
+//  Created by Тагай Абдылдаев on 2023/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
